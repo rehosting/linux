524a1ccf6e64746e999da940525114bd35923e26
Andrew Fasano <fasano@mit.edu>
2023-12-18 09:56:03 -0500

    Hide kernel module errors (init/finit/delete_module return 0). From Firmadyne

--- kernel/module.c
+++ kernel/module.c
@@ -806,7 +806,9 @@ SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
 {
 	struct module *mod;
 	char name[MODULE_NAME_LEN];
-	int ret, forced = 0;
+	int ret = 0, forced = 0;
+
+	return ret;
 
 	if (!capable(CAP_SYS_MODULE) || modules_disabled)
 		return -EPERM;
@@ -3323,6 +3325,8 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 	int err;
 	struct load_info info = { };
 
+	return 0;
+
 	err = may_init_module();
 	if (err)
 		return err;
@@ -3341,6 +3345,8 @@ SYSCALL_DEFINE3(finit_module, int, fd, const char __user *, uargs, int, flags)
 {
 	int err;
 	struct load_info info = { };
+
+	return 0;
 
 	err = may_init_module();
 	if (err)
