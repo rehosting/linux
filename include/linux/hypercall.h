c7f0db26f0eb6de90a7f8d7c1c357d986cee4dac
Andrew Fasano <fasano@mit.edu>
2024-01-05 13:32:42 -0500

    Hypercall: switch to unified unsigned long type (pointer size for both 32 and 64 bit)

--- include/linux/hypercall.h
+++ include/linux/hypercall.h
@@ -2,21 +2,8 @@
 #define HYPERCALL_H
 #include "linux/types.h"
 
-static inline void igloo_hypercall(uint64_t num, uint64_t arg1) {
-#if defined(CONFIG_MIPS) && defined(CONFIG_64BIT)
-    // XXX shouldn't 'unsigned long' type work for both 32 and 64 bit?
-    // Do we actually need both cases?
-    register unsigned long long a0 asm("a0") = num;
-    register unsigned long long a1 asm("a1") = arg1;
-
-    asm volatile(
-       "movz $0, $0, $0"
-        : "+r"(a0)  // Input and output in R0
-        : "r"(a1) // arg1 in register A1
-        : // No clobber
-    );
-
-#elif defined(CONFIG_MIPS) && !defined(CONFIG_64BIT)
+static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
+#if defined(CONFIG_MIPS)
     register unsigned long a0 asm("a0") = num;
     register unsigned long a1 asm("a1") = arg1;
 
@@ -44,7 +31,7 @@ static inline void igloo_hypercall(uint64_t num, uint64_t arg1) {
 #endif
 }
 
-static inline unsigned long igloo_hypercall2(uint64_t num, uint64_t arg1, uint64_t arg2) {
+static inline unsigned long igloo_hypercall2(unsigned long num, unsigned long arg1, unsigned long arg2) {
 #if defined(CONFIG_ARM)
     register unsigned long r0 asm("r0") = num;
     register unsigned long r1 asm("r1") = arg1;
@@ -59,19 +46,7 @@ static inline unsigned long igloo_hypercall2(uint64_t num, uint64_t arg1, uint64
 
     return r0;
 
-#elif defined(CONFIG_MIPS) && defined(CONFIG_64BIT)
-    register unsigned long long a0 asm("a0") = num;
-    register unsigned long long a1 asm("a1") = arg1;
-    register unsigned long long a2 asm("a2") = arg2;
-
-    asm volatile(
-       "movz $0, $0, $0"
-        : "+r"(a0)  // Input and output in R0
-        : "r"(a1) , "r" (a2)// arg1 in register A1
-        : // No clobber
-    );
-
-#elif defined(CONFIG_MIPS) && !defined(CONFIG_64BIT)
+#elif defined(CONFIG_MIPS)
     register unsigned long a0 asm("a0") = num;
     register unsigned long a1 asm("a1") = arg1;
     register unsigned long a2 asm("a2") = arg2;
@@ -79,7 +54,7 @@ static inline unsigned long igloo_hypercall2(uint64_t num, uint64_t arg1, uint64
     asm volatile(
        "movz $0, $0, $0"
         : "+r"(a0)  // Input and output in R0
-        : "r"(a1), "r"(a2) // arg1 in register A1, arg2 in register A2
+        : "r"(a1) , "r" (a2)// arg1 in register A1
         : // No clobber
     );
     return a0;
