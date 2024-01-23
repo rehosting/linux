fbd339cda729b6e62ea0c3d0ca62bc33bb012cc2
Benjamin Levy <benjamin.levy@ll.mit.edu>
2024-01-23 13:20:31 -0500

    Fix copy_to_user() with kernel pointer in dyndev init

--- drivers/dyndev/hyperutils.c
+++ drivers/dyndev/hyperutils.c
@@ -4,33 +4,39 @@
 #include <linux/dyndev.h>
 #include "hyperutils.h"
 
-// Unified read function for hypervisor interaction
-ssize_t hypervisor_read(const char *device_name, char *buffer, size_t len, loff_t *offset) {
-    char *kernel_buffer;
+ssize_t hypervisor_read_kernel(const char *device_name, char *buffer, size_t len, loff_t *offset) {
     struct hyper_file_op hyper_op;
-    ssize_t ret;
     //printk(KERN_INFO "dyndev: read for device %s\n", device_name);
+
+    hyper_op.type = HYPER_READ;
+    strncpy(hyper_op.device_name, device_name, 127);
+    hyper_op.args.read_args.buffer = buffer;
+    hyper_op.args.read_args.length = len;
+    hyper_op.args.read_args.offset = *offset;
+
+    sync_struct(&hyper_op);
+
+    if (hyper_op.rv > 0) {
+        *offset += hyper_op.rv;
+    }
+
+    return hyper_op.rv;
+}
+
+// Unified read function for hypervisor interaction
+ssize_t hypervisor_read(const char *device_name, char __user *buffer, size_t len, loff_t *offset) {
+    char *kernel_buffer;
+    ssize_t ret;
 
     kernel_buffer = kmalloc(len, GFP_KERNEL);
     if (!kernel_buffer) {
         return -ENOMEM;
     }
 
-    hyper_op.type = HYPER_READ;
-    strncpy(hyper_op.device_name, device_name, 127);
-    hyper_op.args.read_args.buffer = kernel_buffer;
-    hyper_op.args.read_args.length = len;
-    hyper_op.args.read_args.offset = *offset;
-
-    sync_struct(&hyper_op);
+    ret = hypervisor_read_kernel(device_name, kernel_buffer, len, offset);
 
     if (copy_to_user(buffer, kernel_buffer, len)) {
         ret = -EFAULT;
-    } else {
-        if (hyper_op.rv > 0) {
-            *offset += hyper_op.rv;
-        }
-        ret = hyper_op.rv;
     }
 
     kfree(kernel_buffer);
--- drivers/dyndev/hyperutils.h
+++ drivers/dyndev/hyperutils.h
@@ -1,2 +1,3 @@
+ssize_t hypervisor_read_kernel(const char *device_name, char *buffer, size_t len, loff_t *offset);
 ssize_t hypervisor_read(const char *device_name, char *buffer, size_t len, loff_t *offset);
 ssize_t hypervisor_write(const char *device_name, const char *buffer, size_t len, loff_t *offset);
