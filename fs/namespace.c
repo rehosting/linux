0e03e3d7714cb683401f0ffaa550554c213109b6
Andrew Fasano <fasano@mit.edu>
2024-02-20 16:39:05 -0500

    Prevent remounting or mounting faked filesystems

--- fs/namespace.c
+++ fs/namespace.c
@@ -3764,7 +3764,6 @@ int path_mount(const char *dev_name, struct path *path,
 		const char *type_page, unsigned long flags, void *data_page)
 {
 	unsigned int mnt_flags = 0, sb_flags;
-	int ret;
 
 	/* Discard magic */
 	if ((flags & MS_MGC_MSK) == MS_MGC_VAL)
@@ -3777,13 +3776,32 @@ int path_mount(const char *dev_name, struct path *path,
 	if (flags & MS_NOUSER)
 		return -EINVAL;
 
-	ret = security_sb_mount(dev_name, path, type_page, flags, data_page);
-	if (ret)
-		return ret;
-	if (!may_mount())
-		return -EPERM;
-	if (flags & SB_MANDLOCK)
-		warn_mandlock();
+	// IGLOO: Prevent guest from remounting one of our custom mounts
+	if (path->dentry && path->dentry->d_sb && path->dentry->d_sb->s_type &&
+		path->dentry->d_sb->s_type->name && path->dentry->d_name.name) {
+
+		// What filesystem type is the *mount point* - not the requested mount, but the filesystem
+		// we're mounting into. e.g., mounting tmpfs into /home/tmp would be of type ext2 if /home is ext2
+		// If we see a mount_type of fuse, it's likely that we're mounting into one of our fuse filesystems
+		const char *mount_type = path->dentry->d_sb->s_type->name;
+		const char *mount_point = path->dentry->d_name.name; // Name of the directory we're mounting (e.g., /dev -> dev)
+
+		//printk(KERN_INFO "Penguin: Requested mount point: %s, Requested mount type: %s, Actual mount type: %s\n",
+		//		mount_point, type_page ? type_page : "NULL", mount_type);
+
+		if (strncmp("hyperfs", mount_type, 4) == 0) {
+			// We're mounting something at a hyperfs mount point. For example, we might be remounting /dev after we've set up our initial hyperfs mount
+			// Let's check that it's one of the paths we care about: sys, dev, or proc. If so, we'll block it. Otherwise continue as normal
+			if (strncmp(mount_point, "dev", 3) == 0 ||
+				strncmp(mount_point, "sys", 3) == 0 ||
+				strncmp(mount_point, "proc", 4) == 0) {
+				//printk(KERN_INFO "Penguin: Blocking attempt to remount %s within a fuse as %s\n", mount_point, type_page ? type_page : "NULL");
+				return 0;
+			}
+		}
+	//} else {
+		//printk(KERN_WARNING "Penguin: Incomplete mount information\n");
+	}
 
 	/* Default to relatime unless overriden */
 	if (!(flags & MS_NOATIME))
