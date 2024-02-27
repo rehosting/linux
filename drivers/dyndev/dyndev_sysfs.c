45d099e3d7e664356d7113f173f70b39ec324c79
Andrew Fasano <fasano@mit.edu>
2024-02-27 11:43:24 -0500

    Bugfix: make dyndev sysfs work

--- drivers/dyndev/dyndev_sysfs.c
+++ drivers/dyndev/dyndev_sysfs.c
@@ -31,7 +31,6 @@ struct kobject **sys_files;
 
 extern struct kernfs_node *sysfs_root_kn;
 
-
 static ssize_t sysfs_show(struct kobject *kobj, 
                 struct kobj_attribute *attr, char *buf){
 
@@ -39,21 +38,37 @@ static ssize_t sysfs_show(struct kobject *kobj,
     const char *path;
     const char *full_path;
     loff_t off = 0;
-    char tmpbuf[128];
+    char tmpbuf[512];
 
     path = kobject_get_path(kobj, GFP_KERNEL);
-    
-    full_path = kmalloc(strlen(path) + strlen(attr->attr.name) + 7, GFP_KERNEL);
+    if (!path) {
+        printk(KERN_EMERG "Failed to get kobject path\n");
+        return -ENOMEM;
+    }
+
+    full_path = kmalloc(strlen(path) + strlen(attr->attr.name) + 7, GFP_KERNEL); // +7 for slash and null terminator + /sys/
+    if (!full_path) {
+        printk(KERN_EMERG "Failed to allocate memory for full_path\n");
+        kfree(path);
+        return -ENOMEM;
+    }
     sprintf(full_path, "/sys%s/%s", path, attr->attr.name);
 
-    // printk(KERN_EMERG "dyndev: sysfs show %p %s\n", kobj, full_path);
-    ret = hypervisor_read(full_path, tmpbuf, 100, &off);
+    ret = hypervisor_read_kernel(full_path, tmpbuf, sizeof(tmpbuf), &off);
+    if (ret < 0) {
+        printk(KERN_EMERG "Failed to read from hypervisor for %s\n", full_path);
+        kfree(path);
+        kfree(full_path);
+        return ret;
+    }
+    tmpbuf[ret] = (char)0;
+    strncpy(buf, tmpbuf, ret);
     kfree(path);
     kfree(full_path);
     return ret;
 }
-static ssize_t sysfs_store(struct kobject *kobj, 
-                struct kobj_attribute *attr,const char *buf, size_t count)
+static ssize_t sysfs_store(struct kobject *kobj,
+                struct kobj_attribute *attr, const char *buf, size_t count)
 {
     ssize_t ret = 0;
     const char *path;
@@ -61,12 +76,18 @@ static ssize_t sysfs_store(struct kobject *kobj,
     loff_t off = 0;
 
     path = kobject_get_path(kobj, GFP_KERNEL);
-    
-    full_path = kmalloc(strlen(path) + strlen(attr->attr.name) + 7, GFP_KERNEL);
+
+    full_path = kmalloc(strlen(path) + strlen(attr->attr.name) + 7, GFP_KERNEL); // +7 for slash and null terminator + /sys/
+    if (!full_path) {
+        printk(KERN_EMERG "Failed to allocate memory for full_path\n");
+        kfree(path);
+        return -ENOMEM;
+    }
     sprintf(full_path, "/sys%s/%s", path, attr->attr.name);
 
-    // printk(KERN_EMERG "dyndev: sysfs store %p %s\n", kobj, full_path);
-    ret =  hypervisor_write(full_path, buf, PAGE_SIZE, &off);
+    //printk(KERN_EMERG "dyndev: sysfs storing %s for path %s\n", buf, full_path);
+    ret =  hypervisor_write_kernel(full_path, buf, count, &off);
+
     kfree(path);
     kfree(full_path);
     return ret;
@@ -74,98 +95,81 @@ static ssize_t sysfs_store(struct kobject *kobj,
 
 struct kobj_attribute sysfs_entry = __ATTR(sysfs_attr, S_IRUGO|S_IWUSR, sysfs_show, sysfs_store);
 
+struct kernfs_node* create_sysfs_dir(struct kernfs_node *parent, char* dir_path) {
+    char *next_token, *delimiter = "/";
 
-static struct kobject *create_sysfs_dir(const char *path) {
+    next_token = strsep(&dir_path, delimiter);
+    //printk(KERN_WARNING "dyndev: Processing directory: %s\n", next_token);
+
+    while (next_token != NULL) {
+        struct kernfs_node *kn;
+        //printk(KERN_EMERG "dyndev: Creating directory: %s under parent\n", next_token);
+        kn = kernfs_find_and_get(parent, next_token);
+
+        if (!kn){
+            struct kobject *kpar;
+            //printk(KERN_EMERG "Did not find parent directory");
+            kpar = kobject_create_and_add(next_token, (struct kobject *)parent->priv);
+            if (!kpar) {
+                printk(KERN_WARNING "dyndev: Failed to create directory: %s\n", next_token);
+                return ERR_PTR(-ENOMEM);
+            }
+            kn = kernfs_find_and_get(parent, next_token);
+        }
+        parent = kn;
+        next_token = strsep(&dir_path, delimiter); // Correctly update next_token
+    }
+    return parent;
+}
+
+
+int create_sysfs_dir_and_file(const char *path) {
     const char *relative_path = path;
-    const char *residual;
-    char *token, *dup_path, *next_token, *delimiter = "/";
-    struct kobject *head = NULL;
+    char *token, *dup_path;
     struct kernfs_node *parent = sysfs_root_kn;
-
-    printk(KERN_EMERG "dyndev: create_sysfs_dir called with path: %s\n", path);
-
-    // Skip the "/sys/" part if present
-    // if (strncmp(path, "/sys/", 5) == 0) {
-    //     relative_path += 5;
-    // }
-    // // check starting kobjects
-    // if (strncmp(path, "module/", 7) == 0) {
-    //     relative_path += 7;
-    //     head = module_kset;
-    // }else if (strncmp(path, "mm/",3) == 0){
-    //     relative_path += 3;
-    //     head = mm_kobj;
-    // }else if (strncmp(path, "hypervisor/",11) == 0){
-    //     relative_path += 11;
-    //     head = hypervisor_kobj;
-    // }else if (strncmp(path, "firmware/",9) == 0){
-    //     relative_path += 9;
-    //     head = firmware_kobj;
-    // }else{
-    //     printk(KERN_EMERG "dyndev: No starting kobject found\n");
-    //     return;
-    // }
-    // parent = head->sd;
+    const char *file_name, *dir_path;
+    int ret = 0;
 
     dup_path = kstrdup(relative_path, GFP_KERNEL);
     if (!dup_path) {
-        //printk(KERN_WARNING "dyndev: Memory allocation failed for residual path\n");
-        return ERR_PTR(-ENOMEM);
+        printk(KERN_WARNING "dyndev: Memory allocation failed for residual path\n");
+        return -ENOMEM;
     }
 
-    token = strsep(&dup_path, delimiter);
-    next_token = strsep(&dup_path, delimiter);
-    while (next_token != NULL && strlen(token) > 0) {
-        printk(KERN_EMERG "dyndev: Creating directory: %s under parent\n", token);
-        struct kernfs_node *kn = kernfs_find_and_get(parent, token);
+    // Split the path into directory path and file name
+    token = dup_path;
+    file_name = strrchr(token, '/');
+    if (file_name) {
+        dir_path = kstrndup(dup_path, file_name - dup_path, GFP_KERNEL);
+        file_name++; // Move past the last '/' to get the file name
 
-        if (!kn){
-            printk(KERN_EMERG "Did not find parent directory");
-            struct kobject *kpar;
-            kpar = kobject_create_and_add(token, (struct kobject *)parent->priv);
-            if (!kpar) {
-                printk(KERN_WARNING "dyndev: Failed to create directory: %s\n", token);
-                kfree(dup_path);
-                return ERR_PTR(-ENOMEM);
-            }
-            kn = kernfs_find_and_get(parent, token);
-            if (!kn) {
-                printk(KERN_WARNING "dyndev: Failed to find directory: %s\n", token);
-                kfree(dup_path);
-                return ERR_PTR(-ENOMEM);
-            }
+        //printk(KERN_EMERG "dyndev: Creating directory %s for file %s\n", dir_path, file_name);
+
+        parent = create_sysfs_dir(parent, (char*)dir_path);
+        kfree(dir_path); // Free the allocated directory path after use
+        if (IS_ERR(parent)) {
+            kfree(dup_path);
+            return PTR_ERR(parent);
         }
-        parent = kn;
-        token = next_token;
-        next_token = strsep(&dup_path, delimiter); // Move to next token
+    } else {
+        printk(KERN_EMERG "Dyndev: UNSUPPORTED bare sysfs file created. This doesn't seem to work! TODO: Debug and fix this");
+        file_name = token;
     }
 
-    struct kobject *par = NULL;
-  
-    par = (struct kobject *)parent->priv;
-    int ret = 0;
-    // Create the sysfs file with the name of the last token
-    if (token != NULL) {
-        printk(KERN_EMERG "dyndev: Creating sysfs file: %s\n", token);
-        sysfs_entry.attr.name = token;
-        ret = sysfs_create_file(par, &sysfs_entry.attr);
+    // Create file_name
+    if (file_name != NULL) {
+        sysfs_entry.attr.name = kstrdup(file_name, GFP_KERNEL);
+        ret = sysfs_create_file((struct kobject *)parent->priv , &sysfs_entry.attr);
         if (ret) {
-            printk(KERN_WARNING "dyndev: Failed to create sysfs file: %s\n", token);
+            printk(KERN_WARNING "dyndev: Failed to create sysfs file: %s\n", file_name);
             kfree(dup_path);
-            return ERR_PTR(-ENOMEM);
+            return -ENOMEM;
         }
-        struct kernfs_node *kfsn = kernfs_find_and_get(parent, token);
-        if (kfsn) {
-            printk(KERN_WARNING "dyndev: Failed to find sysfs file: %s\n", token);
-            kfree(dup_path);
-            return ERR_PTR(-ENOMEM);
-        }
-        par = (struct kobject *)kfsn->priv;
     }
 
     kfree(dup_path);
-    printk(KERN_EMERG "dyndev: Directory and file creation successful, returning entry\n");
-    return par;
+    //printk(KERN_EMERG "dyndev: Directory and file creation successful, returning\n");
+    return 0;
 }
 
 int dyndev_init_sysfs(char *sysfs) {
@@ -201,13 +205,14 @@ int dyndev_init_sysfs(char *sysfs) {
 
     str = sysfs; // Reset str to start of procnames
     while ((token = strsep(&str, ",")) != NULL) {
+        int err;
         if (!(*token)) {
             continue;
         }
-        entry = create_sysfs_dir(token);
+        err = create_sysfs_dir_and_file(token);
         // Check if the entry is an error pointer
-        if (IS_ERR(entry)) {
-            printk(KERN_WARNING "dyndev: Failed to create sysfs file for %s\n", token);
+        if (err < 0) {
+            printk(KERN_WARNING "dyndev: Failed to create sysfs file for %s: %d\n", token, err);
             sys_files[i] = NULL;
         } else {
             sys_files[i] = entry;
