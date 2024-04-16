02cdde05f56d9ece627508ef027a294dc728385c
Andrew Fasano <fasano@mit.edu>
2024-04-16 14:05:12 -0400

    Add support for hypercalls

---
+++ include/linux/hypercall.h
@@ -0,0 +1,174 @@
+#ifndef HYPERCALL_H
+#define HYPERCALL_H
+#include "linux/types.h"
+
+static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
+#if defined(CONFIG_MIPS)
+    register unsigned long reg0 asm("v0") = num;
+    register unsigned long reg1 asm("a0") = arg1;
+
+    asm volatile(
+       "movz $0, $0, $0"
+        : "+r"(reg0)
+        : "r"(reg1) // num in register v0
+        : "memory"
+    );
+
+
+#elif defined(CONFIG_ARM64)
+    register unsigned long reg0 asm("x8") = num;
+    register unsigned long reg1 asm("x0") = arg1;
+    asm volatile(
+            "msr S0_0_c5_c0_0, xzr \n"
+            : "+r"(reg1)
+            : "r"(reg0)
+            : "memory"
+        );
+#elif defined(CONFIG_ARM) 
+    register unsigned long reg0 asm("r7") = num;
+    register unsigned long reg1 asm("r0") = arg1;
+
+    asm volatile(
+    "mcr p7, 0, r0, c0, c0, 0"
+      : "+r"(reg1)
+      : "r"(reg0)
+      : "memory"
+  );  
+#elif defined(CONFIG_X86_64)
+    register unsigned long reg0 asm("rax") = num;
+    register unsigned long reg1 asm("rdi") = arg1;
+
+    asm volatile(
+        "cpuid"
+        : "+r"(reg0)           // hypercall num + return value in rax
+        : "r"(reg1)            // arguments
+        : "memory", "ebx", "ecx", "edx"  // No clobber
+    );
+#elif defined(CONFIG_LOONGARCH)
+	register unsigned long reg0 asm("a1") = num;
+	register unsigned long reg1 asm("a2") = arg1;
+    
+    asm volatile(
+        "cpucfg $r0, $r0"
+        : "+r"(reg0)
+        : "r"(reg1)
+        : "memory"  // No clobber
+    );
+#elif defined(CONFIG_PPC)
+	register unsigned long reg0 asm("r4") = num;
+	register unsigned long reg1 asm("r5") = arg1;
+
+    asm volatile(
+        "xori 10, 10, 0"
+        : "+r"(reg0) 
+        : "r"(reg1)
+        : "memory"
+    );
+#elif defined(CONFIG_RISCV)
+	register unsigned long reg0 asm("a0") = num;
+	register unsigned long reg1 asm("a1") = arg1;
+
+    asm volatile(
+        "xori x0, x0, 0"
+        : "+r"(reg0)
+        : "r"(reg1)
+        : "memory"
+    );
+#else
+#error "No igloo_hypercall support for architecture"
+#endif
+}
+
+static inline unsigned long igloo_hypercall2(unsigned long num, unsigned long arg1, unsigned long arg2) {
+#if defined(CONFIG_ARM64)
+    register unsigned long reg0 asm("x8") = num;
+    register unsigned long reg1 asm("x0") = arg1;
+    register unsigned long reg2 asm("x1") = arg2;
+    asm volatile(
+       "msr S0_0_c5_c0_0, xzr \n"
+        : "+r"(reg1)  // Input and output
+        : "r"(reg0), "r"(reg2)
+        : "memory"
+    );
+    return reg1;
+#elif defined(CONFIG_ARM)
+    register unsigned long reg0 asm("r7") = num;
+    register unsigned long reg1 asm("r0") = arg1;
+    register unsigned long reg2 asm("r1") = arg2;
+
+    asm volatile(
+       "mcr p7, 0, r0, c0, c0, 0"
+        : "+r"(reg1)  // Input and output
+        : "r"(reg0), "r"(reg2)
+        : "memory"
+    );
+
+    return reg1;
+
+#elif defined(CONFIG_MIPS)
+    register unsigned long reg0 asm("v0") = num;
+    register unsigned long reg1 asm("a0") = arg1;
+    register unsigned long reg2 asm("a1") = arg2;
+
+    asm volatile(
+       "movz $0, $0, $0"
+        : "+r"(reg0)  // Input and output in R0
+        : "r"(reg1) , "r" (reg2)// arg2 in register A1
+        : "memory"
+    );
+    return reg0;
+#elif defined(CONFIG_X86_64)
+    register unsigned long reg0 asm("rax") = num;
+    register unsigned long reg1 asm("rdi") = arg1;
+    register unsigned long reg2 asm("rsi") = arg2;
+
+    asm volatile(
+        "cpuid"
+        : "+r"(reg0)           // hypercall num + return value in rax
+        : "r"(reg1), "r"(reg2) // arguments
+        :  "memory", "ebx", "ecx", "edx"
+    );
+
+    return reg0;
+#elif defined(CONFIG_LOONGARCH)
+	register unsigned long reg0 asm("a1") = num;
+	register unsigned long reg1 asm("a2") = arg1;
+	register unsigned long reg2 asm("a3") = arg2;
+    
+    asm volatile(
+        "cpucfg $r0, $r0"
+        : "+r"(reg0)
+        : "r"(reg1), "r"(reg2)
+        : "memory"  // No clobber
+    );
+    return reg0;
+#elif defined(CONFIG_PPC)
+	register unsigned long reg0 asm("r4") = num;
+	register unsigned long reg1 asm("r5") = arg1;
+	register unsigned long reg2 asm("r6") = arg2;
+
+    asm volatile(
+        "xori 10, 10, 0"
+        : "+r"(reg0)
+        : "r"(reg1), "r" (reg2) 
+        : "memory"
+    );
+    return reg0;
+#elif defined(CONFIG_RISCV)
+	register unsigned long reg0 asm("a0") = num;
+	register unsigned long reg1 asm("a1") = arg1;
+	register unsigned long reg2 asm("a2") = arg2;
+
+    asm volatile(
+        "xori x0, x0, 0"
+        : "+r"(reg0)
+        : "r"(reg1), "r" (reg2) 
+        : "memory"
+    );
+    return reg0;
+#else
+#error "No igloo_hypercall2 support for architecture"
+#endif
+}
+
+#endif
