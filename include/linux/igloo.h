28ff8ae226dc7a339b4ca9717ff0dc4cf07b7030
Andrew Fasano <fasano@mit.edu>
2024-04-16 14:25:02 -0400

    Add option to disable hypercalls

--- include/linux/igloo.h
+++ include/linux/igloo.h
@@ -1,9 +1,8 @@
-// File: include/linux/igloo.h
-
 #ifndef _LINUX_IGLOO_H
 #define _LINUX_IGLOO_H
 
 extern unsigned long igloo_task_size; // mmap.c
+extern bool igloo_do_hc; // mmap.c
 
 #endif /* _LINUX_IGLOO_H */
 
