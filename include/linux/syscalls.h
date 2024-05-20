dd8e8e246705b1f3f3f733623a29f8bbce3d8a82
Luke Craig <lacraig3@gmail.com>
2024-05-20 18:26:37 -0400

    turn off log filtering for arm64

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -259,6 +259,7 @@ struct igloo_sysret {
 		igloo_i++;								\
 	})
 
+#ifndef CONFIG_ARM64
 #define __IGLOO_SHOULD_LOG_SC(name)					\
 	(								\
 		ret == -ENOENT						\
@@ -267,6 +268,12 @@ struct igloo_sysret {
 		 && __syscall_meta_##name.syscall_nr != __NR_ioctl	\
 		 && __syscall_meta_##name.syscall_nr != __NR_close	\
 	)
+#else
+#define __IGLOO_SHOULD_LOG_SC(name) \
+	( \
+		ret == -ENOENT \
+	)
+#endif
 
 #define __IGLOO_LOG_SC(x, name, ...)					\
 	do {								\
