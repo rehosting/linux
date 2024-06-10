c97fbd3c6f7eb9d0e147c64fa7dcf32ec591b6db
Caden Kline <caden.kline@ll.mit.edu>
2024-06-10 15:52:08 -0400

    Switch to syscall abi to support new hypercall plugin.

--- include/linux/hypercall.h
+++ include/linux/hypercall.h
@@ -4,38 +4,35 @@
 
 static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
 #if defined(CONFIG_MIPS)
-    register unsigned long a0 asm("a0") = num;
-    register unsigned long a1 asm("a1") = arg1;
+    register unsigned long v0 asm("v0") = num;
+    register unsigned long a0 asm("a0") = arg1;
 
     asm volatile(
        "movz $0, $0, $0"
         : "+r"(a0)  // Input and output in R0
-        : "r"(a1) // arg1 in register A1
+        : "r"(v0), // num in register v0
         : // No clobber
     );
 
 
 #elif defined(CONFIG_ARM)
-  register uint32_t r0 asm("r0") = num;
-  register uint32_t r1 asm("r1") = arg1;
+  #if defined(CONFIG_ARM64)
+  register uint32_t reg0 asm("x8") = num;
+  register uint32_t reg1 asm("x0") = arg1;
+  #else
+  register uint32_t reg0 asm("r7") = num;
+  register uint32_t reg1 asm("r0") = arg1;
+  #endif
+  
+
   asm volatile(
      "mov r0, %0 \t\n\
       mov r1, %1 \t\n\
       mcr p7, 0, r0, c0, c0, 0"
       :
-      : "r"(r0), "r"(r1)
+      : "r"(reg0), "r"(reg1)
       :
   );
-#elif defined(CONFIG_ARM64)
-    register unsigned long x0 asm("x0") = num;
-    register unsigned long x1 asm("x1") = arg1;
-
-    asm volatile(
-        "msr S0_0_c5_c0_0, xzr \n"
-        : "+r"(x0) // Input and output
-        : "r"(x1)
-        : // No clobber
-    );
 #else
 #error "No igloo_hypercall support for architecture"
 #endif
@@ -43,40 +40,34 @@ static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
 
 static inline unsigned long igloo_hypercall2(unsigned long num, unsigned long arg1, unsigned long arg2) {
 #if defined(CONFIG_ARM)
-    register unsigned long r0 asm("r0") = num;
-    register unsigned long r1 asm("r1") = arg1;
-    register unsigned long r2 asm("r2") = arg2;
+   #if defined(CONFIG_ARM64)
+  register uint32_t reg0 asm("x8") = num;
+  register uint32_t reg1 asm("x0") = arg1;
+  register uint32_t reg1 asm("x1") = arg2;
+  #else
+  register uint32_t reg0 asm("r7") = num;
+  register uint32_t reg1 asm("r0") = arg1;
+  register uint32_t reg2 asm("r0") = arg2;
+  #endif
 
     asm volatile(
        "mcr p7, 0, r0, c0, c0, 0"
-        : "+r"(r0)  // Input and output
-        : "r"(r1), "r"(r2)
+        : "+r"(reg1)  // Input and output
+        : "r"(reg0), "r"(reg2),
         : // No clobber
     );
 
     return r0;
-#elif defined(CONFIG_ARM64)
-    register unsigned long x0 asm("x0") = num;
-    register unsigned long x1 asm("x1") = arg1;
-    register unsigned long x2 asm("x2") = arg2;
 
-    asm volatile(
-        "msr S0_0_c5_c0_0, xzr \n"
-        : "+r"(x0) // Input and output
-        : "r"(x1), "r"(x2)
-        : // No clobber
-    );
-
-    return x0;
 #elif defined(CONFIG_MIPS)
-    register unsigned long a0 asm("a0") = num;
-    register unsigned long a1 asm("a1") = arg1;
-    register unsigned long a2 asm("a2") = arg2;
+    register unsigned long v0 asm("v0") = num;
+    register unsigned long a0 asm("a0") = arg1;
+    register unsigned long a1 asm("a1") = arg2;
 
     asm volatile(
        "movz $0, $0, $0"
         : "+r"(a0)  // Input and output in R0
-        : "r"(a1) , "r" (a2)// arg1 in register A1
+        : "r"(a1) , "r" (v0)// arg2 in register A1
         : // No clobber
     );
     return a0;
