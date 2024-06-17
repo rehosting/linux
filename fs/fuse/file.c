420e69daeaddc6dcf37c0ec24c414ebf1169b45b
Benjamin Levy <benjamin.levy@ll.mit.edu>
2024-06-17 14:09:33 -0400

    Allow FUSE + direct_io + MAP_SHARED

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2078,10 +2078,6 @@ static int fuse_file_mmap(struct file *file, struct vm_area_struct *vma)
 
 static int fuse_direct_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	/* Can't provide the coherency needed for MAP_SHARED */
-	if (vma->vm_flags & VM_MAYSHARE)
-		return -ENODEV;
-
 	invalidate_inode_pages2(file->f_mapping);
 
 	return generic_file_mmap(file, vma);
