9e22ac5b54eae3c23c2b9dd250db3bd5f833a02a
Caden Kline <caden.kline@ll.mit.edu>
2024-07-01 12:49:18 -0400

    Add the ability to build

--- include/linux/hypercall.h
+++ include/linux/hypercall.h
@@ -10,54 +10,69 @@ static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
     asm volatile(
        "movz $0, $0, $0"
         : "+r"(v0)  // Input and output in R0
-        : "r"(a0), // num in register v0
+        : "r"(a0) // num in register v0
         : // No clobber
     );
 
 
-#elif defined(CONFIG_ARM)
+#elif defined(CONFIG_ARM) || defined(CONFIG_ARM64)
   #if defined(CONFIG_ARM64)
-  register uint32_t reg0 asm("x8") = num;
-  register uint32_t reg1 asm("x0") = arg1;
+    register uint32_t reg0 asm("x8") = num;
+    register uint32_t reg1 asm("x0") = arg1;
+    asm volatile(
+            "msr S0_0_c5_c0_0, xzr \n"
+            : "+r"(reg1) // Input and output
+            : "r"(reg0)
+            : // No clobber
+        );
   #else
-  register uint32_t reg0 asm("r7") = num;
-  register uint32_t reg1 asm("r0") = arg1;
-  #endif
-  
+    register uint32_t reg0 asm("r7") = num;
+    register uint32_t reg1 asm("r0") = arg1;
 
-  asm volatile(
-     "mov r0, %0 \t\n\
-      mov r1, %1 \t\n\
-      mcr p7, 0, r0, c0, c0, 0"
+    asm volatile(
+    "mcr p7, 0, r0, c0, c0, 0"
       :
       : "r"(reg0), "r"(reg1)
       :
   );
+  #endif
+  
+
+
 #else
 #error "No igloo_hypercall support for architecture"
 #endif
 }
 
 static inline unsigned long igloo_hypercall2(unsigned long num, unsigned long arg1, unsigned long arg2) {
-#if defined(CONFIG_ARM)
+#if defined(CONFIG_ARM) || defined(CONFIG_ARM64)
    #if defined(CONFIG_ARM64)
-  register uint32_t reg0 asm("x8") = num;
-  register uint32_t reg1 asm("x0") = arg1;
-  register uint32_t reg1 asm("x1") = arg2;
+    register uint32_t reg0 asm("x8") = num;
+    register uint32_t reg1 asm("x0") = arg1;
+    register uint32_t reg2 asm("x1") = arg2;
+    asm volatile(
+       "msr S0_0_c5_c0_0, xzr \n"
+        : "+r"(reg1)  // Input and output
+        : "r"(reg0), "r"(reg2)
+        : // No clobber
+    );
+
   #else
-  register uint32_t reg0 asm("r7") = num;
-  register uint32_t reg1 asm("r0") = arg1;
-  register uint32_t reg2 asm("r0") = arg2;
-  #endif
+    register uint32_t reg0 asm("r7") = num;
+    register uint32_t reg1 asm("r0") = arg1;
+    register uint32_t reg2 asm("r0") = arg2;
 
     asm volatile(
        "mcr p7, 0, r0, c0, c0, 0"
         : "+r"(reg1)  // Input and output
-        : "r"(reg0), "r"(reg2),
+        : "r"(reg0), "r"(reg2)
         : // No clobber
     );
 
-    return r0;
+  #endif
+
+
+    return reg1;
 
 #elif defined(CONFIG_MIPS)
     register unsigned long v0 asm("v0") = num;
