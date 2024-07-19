09cb7c96032adcaa749adbf48c1638749faef216
Caden Kline <caden.kline@ll.mit.edu>
2024-07-19 14:34:46 -0400

    Ensure that the compiler considers the retval register as clobbered

--- include/linux/hypercall.h
+++ include/linux/hypercall.h
@@ -9,8 +9,8 @@ static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
 
     asm volatile(
        "movz $0, $0, $0"
-        :
-        : "r"(reg0), "r"(reg1) // num in register v0
+        : "+r"(reg0)
+        : "r"(reg1) // num in register v0
         : // No clobber
     );
 
@@ -20,8 +20,8 @@ static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
     register unsigned long reg1 asm("x0") = arg1;
     asm volatile(
             "msr S0_0_c5_c0_0, xzr \n"
-            :
-            : "r"(reg0), "r"(reg1)
+            : "+r"(reg1)
+            : "r"(reg0)
             : // No clobber
         );
   #elif defined(CONFIG_ARM) 
@@ -30,8 +30,8 @@ static inline void igloo_hypercall(unsigned long num, unsigned long arg1) {
 
     asm volatile(
     "mcr p7, 0, r0, c0, c0, 0"
-      :
-      : "r"(reg0), "r"(reg1)
+      : "+r"(reg1)
+      : "r"(reg0)
       :
   );  
 #else
