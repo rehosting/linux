32e77a475fcaf98e0edc4e3a320e4b4b33f68f21
Luke Craig <lacraig3@gmail.com>
2025-03-17 13:00:24 -0400

    fixup init

--- drivers/igloo/igloo_hc.c
+++ drivers/igloo/igloo_hc.c
@@ -7,8 +7,10 @@
 #include <linux/slab.h>
 #include <linux/hypercall.h>
 #include <linux/igloo.h>
+#include <linux/unistd.h>
 #include "vma_hc.h"
 #include "exec_hc.h"
+#include "syscalls_hc.h"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("IGLOO Kernel Inspection/Interventions");
@@ -16,15 +18,16 @@ MODULE_VERSION("0.1");
 
 /* Register probes for mmap and munmap */
 static int __init igloo_hc_init(void) {
+	printk(KERN_EMERG "IGLOO: Initializing\n");
 	int ret = 0;
 	if ((ret = vma_hc_init()) != 0) {
         printk(KERN_ERR "Failed to register vma_hc\n");
 		return ret;
 	}
-	// if ((ret = exec_hc_init()) != 0) {
-    //     printk(KERN_ERR "Failed to register exec_hc\n");
-	// 	return ret;
-	// }
+	if ((ret = syscalls_hc_init()) != 0) {
+		printk(KERN_ERR "Failed to register syscalls_hc returning %d\n", ret);
+		return ret;
+	}
 	return 0;
 }
 
