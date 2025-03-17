b8e9bb9f02d0e70ba992a1df835e15b9e6ac7adb
Luke Craig <lacraig3@gmail.com>
2025-03-17 15:12:52 -0400

    IGLOO module: add ioctl_hc

---
+++ drivers/igloo/ioctl_hc.c
@@ -0,0 +1,53 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/kprobes.h>
+#include <linux/mm.h>
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <linux/hypercall.h>
+#include <linux/igloo.h>
+#include <linux/binfmts.h>
+#include <trace/syscall.h>
+#include <linux/utsname.h>
+
+void igloo_ioctl(int error, struct inode *inode, struct file *filp, unsigned int cmd, void __user * argp);
+
+/**
+ * Called from do_vfs_ioctl in fs/ioctl.c
+ */
+void igloo_ioctl(int error, struct inode *inode, struct file *filp, unsigned int cmd, void __user * argp) {
+    if (!igloo_do_hc){
+	    return;
+    }
+    if (error == -ENOTTY) {
+        char *path;
+        char *path_buffer = kmalloc(PATH_MAX, GFP_KERNEL);
+
+        if (path_buffer == NULL) {
+            // Handle error in allocating memory for path buffer
+            printk(KERN_ERR "IGLOO ioctl: failed to allocate memory for path buffer\n");
+            return;
+        }
+    
+        // Attempt to resolve the file path
+        path = d_path(&filp->f_path, path_buffer, PATH_MAX);
+        if (IS_ERR(path)) {
+            // Handle error in resolving path, maybe log this condition
+            printk(KERN_ERR "IGLOO ioctl: failed to resolve file path\n");
+        } else {
+            // Log the path and the cmd that led to the -ENOTTY error
+            int hrv;
+            while (1) {
+                hrv = igloo_hypercall2(IGLOO_IOCTL_ENOTTY, (unsigned long)path, cmd);
+                if (hrv == 1) {
+                    // Here, ensure path is logged if needed
+                    printk(KERN_INFO "IGLOO ioctl: retry hc- path: %s\n", path);
+                    continue;
+                }
+                break;
+            }
+        }
+        kfree(path_buffer);
+    }
+}
