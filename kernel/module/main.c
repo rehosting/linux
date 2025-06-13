0cd5e9322cf33a47d0984ae9dcc89e04124c6f8e
Zak Estrada <zachary.estrada@ll.mit.edu>
2025-06-13 17:03:58 -0400

    actually allow modules to load

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -737,7 +737,7 @@ SYSCALL_DEFINE2(delete_module, const char __user *, name_user,
 	char buf[MODULE_FLAGS_BUF_SIZE];
 	int ret, forced = 0;
 
-	return 0;
+	return 0; // IGLOO: Leaving this in for now
 
 	if (!capable(CAP_SYS_MODULE) || modules_disabled)
 		return -EPERM;
@@ -3438,7 +3438,8 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 	int err;
 	struct load_info info = { };
 
-	return 0;
+	//IGLOO: undoing Firmadyne "don't load modules" - TODO whitelist
+	//return 0;
 
 	err = may_init_module();
 	if (err)
@@ -3590,7 +3591,8 @@ static int idempotent_init_module(struct file *f, const char __user * uargs, int
 
 SYSCALL_DEFINE3(finit_module, int, fd, const char __user *, uargs, int, flags)
 {
-	return 0;
+	//IGLOO: undoing Firmadyne "don't load modules" - TODO whitelist
+	//return 0;
 	int err = may_init_module();
 	if (err)
 		return err;
