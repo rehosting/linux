3b83928e3406d2106f54ac9d021b8697a8926b08
Luke Craig <lacraig3@gmail.com>
2025-07-03 17:51:58 -0400

    add constants in a few places

--- include/igloo.h
+++ include/igloo.h
@@ -12,9 +12,6 @@ extern bool igloo_block_halt; // reboot.c
 struct user_arg_ptr;  // Forward declaration
 struct syscall_metadata; // Forward declaration for syscall metadata functions
 
-void igloo_hc_open(int dfd, struct filename *tmp, int fd);
-void igloo_exec_succeeded(struct filename *filename, struct user_arg_ptr argv, struct user_arg_ptr envp, struct linux_binprm *bprm);
-void igloo_ioctl(int error, struct file *filp, unsigned int cmd);
 bool igloo_should_block_mount(struct path *path);
 void igloo_sock_release(struct socket *sock);
 void igloo_sock_bind(struct socket *sock, struct sockaddr_storage *address);
@@ -29,7 +26,5 @@ int igloo_get_syscall_metadata_count(void);
 struct syscall_metadata *igloo_get_syscall_metadata_copy(int nr);
 int igloo_get_syscall_metadata_count_copy(void);
 struct syscall_metadata *igloo_get_syscall_metadata_by_index_copy(int index);
-
-#include "igloo_hypercall_consts.h"
 
 #endif /* _LINUX_IGLOO_H */
