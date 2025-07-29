edf67ae379f040eeb367cb3d3aadc3818f8c11ae
Luke Craig <lacraig3@gmail.com>
2025-07-29 14:28:29 -0400

    add igloobase elements

---
+++ drivers/igloobase/igloobase.c
@@ -0,0 +1,28 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include "igloo.h"
+#include "igloobase.h"
+
+
+/* Register probes for mmap and munmap */
+static int __init igloo_base_init(void) {
+	printk(KERN_EMERG "IGLOOBase: Initializing\n");
+	int ret = 0;
+    if ((ret = osi_notifier_init()) != 0) {
+        printk(KERN_ERR "Failed to register osi_notifier_init\n");
+    }
+    if ((ret = syscalls_info_report()) != 0) {
+		printk(KERN_ERR "Failed to register syscalls_hc returning %d\n", ret);
+	}
+	return 0;
+}
+
+/* Unregister probes */
+static void __exit igloo_base_exit(void) {
+    // Unreachable, module is built in
+    printk(KERN_ERR "TODO\n");
+}
+
+module_init(igloo_base_init);
+module_exit(igloo_base_exit);
