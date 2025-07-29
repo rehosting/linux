21fd64b1d6fc2a7f783d44e3fa05185c4f7678e7
Luke Craig <lacraig3@gmail.com>
2025-07-29 14:06:48 -0400

    update weaksyms

--- drivers/igloobase/igloo_weaksyms.c
+++ drivers/igloobase/igloo_weaksyms.c
@@ -16,34 +16,74 @@ igloo_syscall_return_t igloo_syscall_return_hook = NULL;
 EXPORT_SYMBOL(igloo_syscall_enter_hook);
 EXPORT_SYMBOL(igloo_syscall_return_hook);
 
-bool igloo_should_block_mount(struct path *path);
-
-bool __weak igloo_should_block_mount(struct path *path)
+// Function pointer for override
+bool (*igloo_should_block_mount_module)(struct path *path);
+bool igloo_should_block_mount(struct path *path)
 {
-    return false;
+    if (igloo_should_block_mount_module) {
+        return igloo_should_block_mount_module(path);
+    } else {
+        // printk(KERN_INFO "igloo_should_block_mount: default implementation called\n");
+        return false;
+    }
 }
 EXPORT_SYMBOL(igloo_should_block_mount);
 
-void __weak igloo_sock_release(struct socket *sock) { }
+void (*igloo_sock_release_module)(struct socket *sock);
+void igloo_sock_release(struct socket *sock)
+{
+    if (igloo_sock_release_module) {
+        igloo_sock_release_module(sock);
+    } else {
+        // printk(KERN_EMERG "igloo_sock_release: unimplemented\n");
+    }
+}
 EXPORT_SYMBOL(igloo_sock_release);
 
-void __weak igloo_sock_bind(struct socket *sock, struct sockaddr_storage *address) {}
+void (*igloo_sock_bind_module)(struct socket *sock, struct sockaddr_storage *address);
+void igloo_sock_bind(struct socket *sock, struct sockaddr_storage *address)
+{
+    if (igloo_sock_bind_module) {
+        igloo_sock_bind_module(sock, address);
+    } else {
+        // printk(KERN_EMERG "igloo_sock_bind: unimplemented\n");
+    }
+}
 EXPORT_SYMBOL(igloo_sock_bind);
 
-void __weak igloo_hc_newuname(struct new_utsname *name) {}
+void (*igloo_hc_newuname_module)(struct new_utsname *name) = NULL;
+void igloo_hc_newuname(struct new_utsname *name)
+{
+    if (igloo_hc_newuname_module) {
+        igloo_hc_newuname_module(name);
+    } else {
+        // printk(KERN_EMERG "igloo_hc_newuname: unimplemented\n");
+    }
+}
 EXPORT_SYMBOL(igloo_hc_newuname);
 
-void igloo_hc_open(int dfd, const char __user *filename,
-			  struct open_how *how);
-void __weak igloo_hc_open(int dfd, const char __user *filename,
-			   struct open_how *how) {}
+void (*igloo_hc_open_module)(int dfd, const char __user *filename, struct open_how *how);
+void igloo_hc_open(int dfd, const char __user *filename, struct open_how *how)
+{
+    if (igloo_hc_open_module) {
+        igloo_hc_open_module(dfd, filename, how);
+    } else {
+        // printk(KERN_EMERG "igloo_hc_open: unimplemented\n");
+    }
+}
 EXPORT_SYMBOL(igloo_hc_open);
 
-void __weak igloo_ioctl(int error, 
-        struct inode *inode, struct file *filp, 
-        unsigned int cmd, void __user * argp) {}
-
+void (*igloo_ioctl_module)(int error, struct inode *inode, struct file *filp, unsigned int cmd, void __user *argp);
+void igloo_ioctl(int error, struct inode *inode, struct file *filp, unsigned int cmd, void __user *argp)
+{
+    if (igloo_ioctl_module) {
+        igloo_ioctl_module(error, inode, filp, cmd, argp);
+    } else {
+        // printk(KERN_EMERG "igloo_ioctl: unimplemented\n");
+    }
+}
 EXPORT_SYMBOL(igloo_ioctl);
+
 /* Export internal symbols needed for introspection research */
 
 // Symbol lookup functions - now pulled in by trace/syscall.h
