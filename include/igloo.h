df6d9140f3be228800241aa7d2f3c8991914a745
Luke Craig <lacraig3@gmail.com>
2025-07-31 12:17:12 -0400

    drop igloo_log_cov

--- include/igloo.h
+++ include/igloo.h
@@ -5,7 +5,6 @@
 #include <linux/binfmts.h>
 
 extern unsigned long igloo_task_size; // mmap.c
-extern bool igloo_log_cov; // mmap.c
 extern bool igloo_block_halt; // reboot.c
 
 struct user_arg_ptr;  // Forward declaration
