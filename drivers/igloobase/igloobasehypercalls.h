7ff4ef10b827f56d2f6694f79cb4fd7d74ad4f87
Luke Craig <lacraig3@gmail.com>
2025-08-01 11:10:02 -0400

    igloobase: add driver

---
+++ drivers/igloobase/igloobasehypercalls.h
@@ -0,0 +1,6 @@
+// This should be a relatively small file as most functionality should be
+// implemented in the core igloo driver
+
+enum igloo_base_hypercalls {
+    IGLOO_HYP_SETUP_SYSCALL = 0x1337,
+};
